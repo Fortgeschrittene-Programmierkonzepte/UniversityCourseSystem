a623b7d19ee3c0165d39020ca4f32c9c090c4d13
benediktfrickenstein <benediktfrickenstein@users.noreply.github.com>
2025-03-22 16:25:05 +0000

    deploy: d1cc85765598be2511c486bb23a59e68dce7905e

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.proseminar.github","l":"Course"},{"p":"de.proseminar.github","l":"Student"}];updateSearchResults();
